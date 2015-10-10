4cec32059b6b495a29aaed65acce9910fdcb9acb
Aurelien <aurelm38@gmail.com>
2015-10-10 16:55:31 +0200

    modif recherche_thread

--- TP3/recherche_thread.c
+++ TP3/recherche_thread.c
@@ -12,37 +12,37 @@ struct timeval t0;
 struct timeval t1;
 volatile int arret = 0;
 //cat /proc/cpuinfo | grep processor | wc -l
-//Recupere le nombre de processeurs des machines sur lequel la commande est lancer.
+//Recupere le nombre de processeurs des machines sur lequel la commande est lancée.
 
 int tab[SIZE];
 int val = NOMBRE;
 
-int* recherche_debut(){
+void* recherche_debut(void *arg){
 	int i;
 	for (i = 0; i < SIZE/2 && arret == 0; i++){
 		if (tab[i] == val){
 			arret = 1;
-			return (int*) i;
+			pthread_exit((void*)i);
 		}
 	}
-	return -1;
+	pthread_exit((void*)-1);
 }
 
-int* recherche_fin(){
+void* recherche_fin(void *arg){
 	int i;
 	for (i = SIZE-1; i >= SIZE/2 && arret == 0; i--){
 		if (tab[i] == val){
 			arret = 1;
-			return (int*) i;
+			pthread_exit((void*)i);
 		}
 	}
-	return -1;
+	pthread_exit((void*)-1);
 }
 
 int main (int argc, char **argv){
 	pthread_t tids[2];
 	int i, retour=-1;
-	int status = 0;
+	void* status;
 	for (i = 0; i < SIZE; i++){
 		tab[i] = (float) rand() / RAND_MAX * SIZE;
 	}
@@ -50,14 +50,15 @@ int main (int argc, char **argv){
 	pthread_create (&tids[0], NULL, recherche_debut, NULL);
 	pthread_create (&tids[1], NULL, recherche_fin, NULL);
 	for (i = 0; i < 2; i++){
-	  pthread_join (tids[i], &status);
-	  if (status != 0){
-	  	retour = status;
+	  pthread_join (tids[i], (void *) &status);
+	  if (status != (int) -1){
+	  	retour = (int) status;
 	  }
+	  printf("Valeur de retour de la thread %lu : [%d]\n",(unsigned long)tids[i],(int)status);
 	}
 	gettimeofday(&t1, 0);
 	if (retour != -1){
-		printf("Trouvé : %d est en %d\n",NOMBRE, retour);
+		printf("Trouvé : %d est en %d\n",NOMBRE, (int)retour);
 	} else {
 		printf("%d n'est pas dans le tableau !!!!!!!!!!!!\n",NOMBRE);
 	}
