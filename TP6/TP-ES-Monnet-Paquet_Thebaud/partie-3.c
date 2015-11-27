46b36c470946ec062eb50c051e26ea72dd755983
Aurelien <aurelm38@gmail.com>
2015-11-27 12:31:35 +0100

    ajout archive et tp7

---
+++ TP6/TP-ES-Monnet-Paquet_Thebaud/partie-3.c
@@ -0,0 +1,22 @@
+#include "stdes.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+
+int main(int argc, char **argv) { 
+	FICHIER *f1;//*f2 : unused variable
+	if(argc != 2) exit(-1);
+
+	//On a du modifier cette ligne car argv[2] est faux.
+	//ligne originale : f1 = ouvrir (argv[2], 'E');
+	//Sachant que argc == 2 prendre argv[2] ne sert pas à grand chose .....
+	f1 = ouvrir(argv[1], 'E');
+	if(f1 == NULL) exit(-1);
+
+	fecriref (f1, " %c %s 12\n", 'a', "bonjour");
+	fecriref (f1, " %d \n", -1257);
+
+	fermer(f1);
+	return 0;
+}
